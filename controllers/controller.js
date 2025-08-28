34161269868fb316d309bc8fcc68a7299e0d5aea
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-28 22:31:46 +0700

    controller showlabels()

---
+++ controllers/controller.js
@@ -0,0 +1,17 @@
+const Model = require("../models/model");
+const View = require("../views/view");
+
+class Controller {
+    //!====lABELS====
+    static async showLabels(req, res) {
+        try {
+            let data = await Model.getLabelsdata();
+            res.render("labels", { data });
+            // res.send(data);
+        } catch (error) {
+            res.send(error);
+            // console.log(error);
+        }
+    }
+}
+module.exports = Controller;
