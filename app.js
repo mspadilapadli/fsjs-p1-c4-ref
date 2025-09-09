1b4f8a16ad481df0d83b623c421489c39d263366
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-09-09 22:12:49 +0700

    router index test

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const express = require("express"); //* panggil si express nya dengan cara impor
 const app = express(); //* buat app untuk menjalankan expressnya,
 const port = 3000; //* tentukan portnya, port general 3000
 
-//buat route / endpoint nya, route form =
-//? app.METHOD(PATH, HANDLER)
-
-//set view engine ejsnya
-app.set("view engine", "ejs");
-
-//*untuk get data dari HTMl
-app.use(express.urlencoded({ extended: true }));
-
 //use router
 app.use("/", require("./routers/index"));
 
