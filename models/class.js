97114de9695ddf49d13b01c4b9cb7b4eab86d022
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-09-10 09:34:34 +0700

    factory class

---
+++ models/class.js
@@ -0,0 +1,103 @@
+class Label {
+    constructor(id, name, since, city) {
+        this.id = id;
+        this.name = name;
+        this.since = since;
+        this.city = city;
+    }
+}
+
+class Song {
+    constructor(
+        id,
+        title,
+        bandName,
+        duration,
+        genre,
+        createdDate,
+        lyric,
+        imageUrl,
+        totalVote,
+        lableId
+    ) {
+        this.id = id;
+        this.title = title;
+        this.bandName = bandName;
+        this.duration = duration;
+        this.genre = genre;
+        this.createdDate = createdDate;
+        this.lyric = lyric;
+        this.imageUrl = imageUrl;
+        this.totalVote = totalVote;
+        this.lableId = lableId;
+    }
+}
+
+class Factory {
+    static instanceLabels(arr) {
+        return arr.map(({ id, name, since, city }) =>
+            this.instanceLabel(id, name, since, city)
+        );
+    }
+
+    static instanceSongs(arr) {
+        return arr.map(
+            ({
+                id,
+                title,
+                bandName,
+                duration,
+                genre,
+                createdDate,
+                lyric,
+                imageUrl,
+                totalVote,
+                lableId,
+            }) =>
+                this.createSong(
+                    id,
+                    title,
+                    bandName,
+                    duration,
+                    genre,
+                    createdDate,
+                    lyric,
+                    imageUrl,
+                    totalVote,
+                    lableId
+                )
+        );
+    }
+
+    static instanceLabel(id, name, since, city) {
+        return new Label(id, name, since, city);
+    }
+
+    static instranceSong(
+        id,
+        title,
+        bandName,
+        duration,
+        genre,
+        createdDate,
+        lyric,
+        imageUrl,
+        totalVote,
+        lableId
+    ) {
+        return new Song(
+            id,
+            title,
+            bandName,
+            duration,
+            genre,
+            createdDate,
+            lyric,
+            imageUrl,
+            totalVote,
+            lableId
+        );
+    }
+}
+
+module.exports = Factory;
