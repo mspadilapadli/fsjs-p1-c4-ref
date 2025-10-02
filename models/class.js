31da852e4e53751b5f29bd04098727deeaca6c7b
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-10-02 10:15:57 +0700

    release 1:  create class

--- models/class.js
+++ models/class.js
@@ -1,3 +1,5 @@
+//* release 1 : create class
+
 class Label {
     constructor(id, name, since, city) {
         this.id = id;
@@ -7,7 +9,34 @@ class Label {
     }
 }
 
+class LabelDetailDuration extends Label {
+    constructor(
+        id,
+        name,
+        since,
+        city,
+        averageDuration,
+        minDuration,
+        maxDuration
+    ) {
+        super(id, name, since, city);
+        this.averageDuration = averageDuration;
+        this.minDuration = minDuration;
+        this.maxDuration = maxDuration;
+    }
+}
 class Song {
+    constructor(id, title, bandName, duration, genre, totalVote) {
+        this.id = id;
+        this.title = title;
+        this.bandName = bandName;
+        this.duration = duration;
+        this.genre = genre;
+        this.totalVote = totalVote;
+    }
+}
+
+class SongDetail extends Song {
     constructor(
         id,
         title,
@@ -18,29 +47,59 @@ class Song {
         lyric,
         imageUrl,
         totalVote,
-        lableId
+        labelId
     ) {
-        this.id = id;
-        this.title = title;
-        this.bandName = bandName;
-        this.duration = duration;
-        this.genre = genre;
+        super(id, title, bandName, duration, genre, totalVote);
         this.createdDate = createdDate;
         this.lyric = lyric;
         this.imageUrl = imageUrl;
-        this.totalVote = totalVote;
-        this.lableId = lableId;
+        this.labelId = labelId;
     }
 }
 
 class Factory {
+    //*Labels
     static instanceLabels(arr) {
         return arr.map(({ id, name, since, city }) =>
             this.instanceLabel(id, name, since, city)
         );
     }
+    static instanceLabel(id, name, since, city) {
+        return new Label(id, name, since, city);
+    }
+
+    static instanceLabelDetailDuration(arr) {
+        return arr.map(
+            ({
+                id,
+                name,
+                since,
+                city,
+                averageDuration,
+                minDuration,
+                maxDuration,
+            }) =>
+                new LabelDetailDuration(
+                    id,
+                    name,
+                    since,
+                    city,
+                    averageDuration,
+                    minDuration,
+                    maxDuration
+                )
+        );
+    }
+
+    //* Songs
 
     static instanceSongs(arr) {
+        return arr.map(
+            ({ id, title, bandName, genre, totalVote }) =>
+                new Song(id, title, bandName, genre, totalVote)
+        );
+    }
+    static instanceSongDetails(arr) {
         return arr.map(
             ({
                 id,
@@ -54,7 +113,7 @@ class Factory {
                 totalVote,
                 lableId,
             }) =>
-                this.instanceSong(
+                new SongDetail(
                     id,
                     title,
                     bandName,
@@ -66,36 +125,6 @@ class Factory {
                     totalVote,
                     lableId
                 )
-        );
-    }
-
-    static instanceLabel(id, name, since, city) {
-        return new Label(id, name, since, city);
-    }
-
-    static instanceSong(
-        id,
-        title,
-        bandName,
-        duration,
-        genre,
-        createdDate,
-        lyric,
-        imageUrl,
-        totalVote,
-        lableId
-    ) {
-        return new Song(
-            id,
-            title,
-            bandName,
-            duration,
-            genre,
-            createdDate,
-            lyric,
-            imageUrl,
-            totalVote,
-            lableId
         );
     }
 }
