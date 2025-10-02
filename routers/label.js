ea651d4d2ac77bdc33a335c387f748b5b4e4b1c8
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-10-02 10:26:13 +0700

    release 1 : create router

--- routers/label.js
+++ routers/label.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const Controller = require("../controllers/controller");
 
 router.get("/", Controller.showLabels);
+router.get("/detail", Controller.showLabelDetail);
 
 module.exports = router;
--- routers/song.js
+++ routers/song.js
@@ -4,4 +4,21 @@ const Controller = require("../controllers/controller");
 
 router.get("/", Controller.showSongs);
 
+//add
+router.get("/add", Controller.showFormAddSong);
+router.post("/add", Controller.postSong);
+
+//update
+router.post("/:id/edit", Controller.postUpdateSong);
+router.get("/:id/edit", Controller.showFormUpdateSong);
+
+//delete
+router.get("/:id/delete", Controller.deteleSong);
+
+// vote song
+router.get("/:id/vote", Controller.vote);
+
+//show single song
+router.get("/:id", Controller.showSongById);
+
 module.exports = router;
