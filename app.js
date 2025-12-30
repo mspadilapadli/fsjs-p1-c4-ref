36376c5e0cf1576bbd6d1ba3d15d19f1eef3d73e
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-12-30 09:35:48 +0700

    R3-app() : urlencode must define before router

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const port = 3000; //* tentukan portnya, port general 3000
 //set view engine
 app.set("view engine", "ejs");
 
+//get data from htlm, wajib sebelum router
+app.use(express.urlencoded({ extended: true }));
+
 //use router
 app.use("/", require("./routers/index"));
-
-//get data from htlm
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
